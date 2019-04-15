1d3a0b58a250445bdd09922e89b80dc4224a91bb
Thomas Yoho <TYohoJr@Gmail.com>
2019-04-14 20:16:20 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   constructor() {
@@ -12,14 +13,25 @@ class App extends Component {
   componentWillMount() {
     let data = require('./assets/1athletes.json');
     console.log(data);
+    this.setState({
+      buttons: data[0].Sports.map((item, i) => {
+        return <Button key={i} className='sport-data-title-btn' onClick={() => console.log(item.SportData.Title)}>{item.SportData.Title.toUpperCase()}</Button>
+      })
+    })
   }
 
   render() {
     return (
       <div className="App">
         <img src={require('./assets/logo-sm.png')} alt='logo-sm' id='logo-sm' />
-        <p className='text'>SELECT ACCOMPLISHMENT</p>
-        {this.state.buttons}
+        <p className='text' id='header-text'>SELECT ACCOMPLISHMENT:</p>
+        <div id='btns-container'>
+          {this.state.buttons}
+        </div>
+        <footer>
+          <div className='text' id='footer-page-navigation'>HOME <span id='footer-text-divider'>></span> WRESTLERS</div>
+          <div id='footer-touchpros'>TouchWall by TouchPros.com</div>
+        </footer>
       </div>
     );
   }
